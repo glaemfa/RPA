13dacf615f701ff7cb11e6aded94b627f3f6eb85
glaemfa <yeji040320@gmail.com>
2024-09-12 16:13:01 +0900

    2주차 실습파일

---
+++ 2week/ex2.py
@@ -0,0 +1,17 @@
+a = 123
+b = 15.556
+
+print("a:{0} b:{1}". format(a,b))
+print(f"a{a} b:{b}")
+
+print(f"a:{a:05d} b:{b:2f}")
+print("a:%05d b:%.2f" % (a,b))
+
+print("2개의 숫자를 입력하세요.")
+num1 = int(input())
+num2 = int(input())
+num3 = num1 + num2
+print(num3)
+
+num4 = num1 //3
+print("num1 //3 ",num4)
