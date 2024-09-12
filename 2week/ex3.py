d128fc081ff7127e6dd86a27f5d4cd9719cad84a
glaemfa <yeji040320@gmail.com>
2024-09-12 16:24:33 +0900

    2주차 실습파일

--- 2week/ex3.py
+++ 2week/ex3.py
@@ -1,6 +1,13 @@
-num = int(input("정수를 입력하세요: "))
+a =5
 
-if num % 2 == 0:
-    print("짝수이다 - True")
-else:
-    print("짝수이다 - False")
+if a == 5:
+    print('Right!')
+    print('a is 5')
+    
+if a == 3 :
+    print('Right!')
+    print('a is 3')
+    
+if a !=3:
+    print('Right!')
+    print('a is not 3')
