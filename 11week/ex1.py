4cd3c069e570b724598ff0f4449ebb38172b31cb
glaemfa <yeji040320@gmail.com>
2024-11-28 15:42:56 +0900

    12주차 실습파일

--- 11week/ex1.py
+++ 11week/ex1.py
@@ -2,7 +2,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.formula.api as smf
 
-w = pd.read_csv('ch5-1(1).csv')
+w = pd.read_csv("ch5-1(1).csv")
 w_n = w.iloc[:,1:5]
 
 model_lm = smf.ols(formula='weight ~ food',data=w_n)
